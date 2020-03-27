f2b1b549f8dccfdb51fb253843393c07931dbec7
jspeed-meyers <johnmeyersster@gmail.com>
2020-03-26 19:29:49 -0700

    Added in orca

--- ip2map.py
+++ ip2map.py
@@ -46,17 +46,15 @@ class IP2Map():
 
 		return geo_list
 
-
-		
 	def coord2map(self):
 
+		# TODO: Add in functionality of taking image coordinates
+		# Will need to dig into plotly
 		df = px.data.gapminder().query("year == 2007")
 		fig = px.scatter_geo(df, locations="iso_alpha",
                      size="pop", # size of markers, "pop" is one of the columns of gapminder
                      )
-		fig.write_image("fig1.png")
-
-		# TODO: Now I need some goddamn orca library. JS barf
+		fig.write_image("images/ip_map.png")
 
 if __name__ == "__main__":
 
@@ -64,10 +62,3 @@ if __name__ == "__main__":
 	test = IP2Map(ip_list)
 	print(test.coord_list)
 	test.coord2map()
-
-# TODO
-# NUMBER 2. Place lat-longs on map
-
-# plotly
-
-
