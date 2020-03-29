e056b127275bd887cc9a747caf82720393491f30
jspeed-meyers <johnmeyersster@gmail.com>
2020-03-29 16:36:08 -0700

    Added setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+""" File to simplify distributing package """
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+   name='pcap2map',
+   version='0.0.1',
+   description='Put IP addresses from PCAP on map',
+   author='jsm',
+   author_email='johnmeyersster@gmail.com',
+   packages=['pcap2map'],
+   classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+   python_requires='>=3.7.6',
+   install_requires=['pandas', 'plotly', 'pyshark'],
+)
