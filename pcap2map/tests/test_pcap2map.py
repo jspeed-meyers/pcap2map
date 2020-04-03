8bb4159d1d149fb3368a29aeadab24d031f4bd85
jspeed-meyers <johnmeyersster@gmail.com>
2020-04-02 18:34:42 -0700

    Added method tests

--- pcap2map/tests/test_pcap2map.py
+++ pcap2map/tests/test_pcap2map.py
@@ -6,41 +6,89 @@ import pcap2ip
 import ip2map
 import pcap2map
 
-
-# TODO: Create classes for tests
 # TODO: Test individual class methods
 
-def test_pcap2ip():
-    """Test entire pcap2ip pipeline"""
 
-    test = pcap2ip.Pcap2IP("tests/test.pcap")
-    assert test.ips == ['1.1.23.3', '1.1.12.1']
+class TestPcap2ip:
+    """Test pipeline and functions of pcap2ip.py"""
 
-def test_ip2map():
-    """Test entire ip2map pipeline"""
+    def test_pcap2ip(self):
+        """Test entire pcap2ip pipeline"""
 
-    # Set command line arguments for testing purposes
-    sys.argv = ['pcap2ip.py', 'tests/test.pcap']
+        test = pcap2ip.Pcap2IP("tests/test.pcap")
+        assert test.ips == ['1.1.23.3', '1.1.12.1']
 
-    ip_list_test = pcap2ip.Pcap2IP("tests/test.pcap").ips
-    test = ip2map.IP2Map(ip_list_test)
+    def test_ingest_pcap(self):
+        """Test ingest_pcap function"""
 
-    assert test.coord_list == [[23.116671, 113.25], [23.116671, 113.25]]
+        test = pcap2ip.Pcap2IP("tests/test.pcap")
+        test.ingest_pcap()
 
-    test.coord2map('tests/test.png')
+    def test_ip_list(self):
+        """Test ip_list function"""
 
-def test_pcap2map():
-    """Test pcap2map entire pipeline"""
+        test = pcap2ip.Pcap2IP("tests/test.pcap")
+        assert test.ip_list() == ['1.1.23.3',
+                                  '1.1.12.1']
 
-    # Set command line arguments for testing purposes
-    sys.argv = ['pcap2map.py', 'tests/test.pcap']
 
-    test = pcap2map.Pcap2Map()
+class TestIP2map:
+    """Test pipeline and functions of ip2map.py"""
 
-    # Get command line arguments
-    FILE, PNG_PATH = test.get_args()
+    def test_ip2map(self):
+        """Test entire ip2map pipeline"""
 
-    # Run main function
-    test.run(FILE, PNG_PATH)
+        # Set command line arguments for testing purposes
+        sys.argv = ['pcap2ip.py', 'tests/test.pcap']
+
+        ip_list_test = pcap2ip.Pcap2IP("tests/test.pcap").ips
+        test = ip2map.IP2Map(ip_list_test)
+
+        assert test.coord_list == [[23.116671, 113.25], [23.116671, 113.25]]
+
+        test.coord2map('tests/test.png')
+
+    def test_ip2coord(self):
+        """Test ip2coord function"""
+
+        # Set command line arguments for testing purposes
+        sys.argv = ['pcap2ip.py', 'tests/test.pcap']
+
+        ip_list_test = pcap2ip.Pcap2IP("tests/test.pcap").ips
+        test = ip2map.IP2Map(ip_list_test)
+
+        assert test.ip2coord() == [[23.116671, 113.25], 
+                                   [23.116671, 113.25]]
+
+    def test_coord2map(self):
+        """Test coord2map function"""
+
+        # Set command line arguments for testing purposes
+        sys.argv = ['pcap2ip.py', 'tests/test.pcap']
+
+        ip_list_test = pcap2ip.Pcap2IP("tests/test.pcap").ips
+        test = ip2map.IP2Map(ip_list_test)
+
+        test.coord2map('tests/test.png')
+
+
+class TestPcap2Map:
+
+    def test_pcap2map():
+        """Test pcap2map entire pipeline"""
+
+        # Set command line arguments for testing purposes
+        sys.argv = ['pcap2map.py', 'tests/test.pcap']
+
+        test = pcap2map.Pcap2Map()
+
+        # Get command line arguments
+        FILE, PNG_PATH = test.get_args()
+
+        # Run main function
+        test.run(FILE, PNG_PATH)
+
+    def function():
+        pass
 
 
