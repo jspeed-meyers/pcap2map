25adfe33c8caefda34e81035e4c92f22a126ce52
jspeed-meyers <johnmeyersster@gmail.com>
2020-04-04 17:10:13 -0700

    Change to src folder structure

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
 """ File to simplify distributing package """
 
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
    name='pcap2map',
-   version='0.0.1',
+   version='0.0.4',
    description='Put IP addresses from PCAP on map',
+   keywords="pcap networking IP geolocations",
    author='anon',
    author_email='anon@gmail.com',
+   url="https://www.google.com",
    long_description=long_description,
    long_description_content_type="text/markdown",
-   packages=setuptools.find_packages(),
+   include_package_data=True,
+   packages=find_packages("src"),
+   package_dir={"": "src"},
    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: System :: Networking",
-        "Intended Audience :: System Administrators"],
-   python_requires='>=3.7.6'
+        "Intended Audience :: System Administrators",
+        ],
+   python_requires='>=3.7.6',
+   install_requires=['pandas',
+                     'plotly',
+                     'pyshark'],
 )
